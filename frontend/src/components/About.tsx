d0706da6f6701b9febe1c8da2ac5902a06ac828c
Paulo Santiago <p.santiago796@gmail.com>
2023-09-24 18:00:32 -0400

    update About text

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -2,7 +2,17 @@ import "../styles/About.scss";
 
 const About = () => {
   return(
-    <h1>About</h1>
+    <div>
+      <h1>About</h1>
+      <p>
+        Hello! My name is Paul and this is a side project where I am learning React TypeScript for the frontend
+        and Django for the backend.
+        <br></br>
+        This project is a text-based chatbot using the ChatGPT API.
+        <br></br>
+        <a href="https://github.com/prsanti/chatbot-sample" target="_blank" rel="noopener noreferrer">Click here to look at this project on Github</a>
+      </p>
+    </div>  
   )
 }
 
